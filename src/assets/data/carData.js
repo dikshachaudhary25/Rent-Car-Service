ad90b65d0690ee944099479f6eb846947348cd68
dikshachaudhary25 <97454110+dikshachaudhary25@users.noreply.github.com>
2024-04-18 12:17:08 +0530

    Add files via upload

---
+++ src/assets/data/carData.js
@@ -0,0 +1,141 @@
+// import all images from assets/images directory
+import img01 from "../all-images/cars-img/nissan-offer.png";
+import img02 from "../all-images/cars-img/offer-toyota.png";
+import img03 from "../all-images/cars-img/bmw-offer.png";
+import img04 from "../all-images/cars-img/nissan-offer.png";
+import img05 from "../all-images/cars-img/offer-toyota.png";
+import img06 from "../all-images/cars-img/mercedes-offer.png";
+import img07 from "../all-images/cars-img/toyota-offer-2.png";
+import img08 from "../all-images/cars-img/mercedes-offer.png";
+
+const carData = [
+  {
+    id: 1,
+    brand: "Tesla",
+    rating: 112,
+    carName: "Tesla Malibu",
+    imgUrl: img01,
+    model: "Model 3",
+    price: 50,
+    speed: "20kmpl",
+    gps: "GPS Navigation",
+    seatType: "Heated seats",
+    automatic: "Automatic",
+    description:
+      " Experience the future of driving with the Tesla Malibu, now available for rent! This all-electric vehicle combines style, performance, and eco-friendliness, making it an excellent choice for your next local or long-distance journey.",
+  },
+
+  {
+    id: 2,
+    brand: "Toyota",
+    rating: 102,
+    carName: "Toyota Aventador",
+    imgUrl: img02,
+    model: "Model-2022",
+    price: 50,
+    speed: "20kmpl",
+    gps: "GPS Navigation",
+    seatType: "Heated seats",
+    automatic: "Automatic",
+    description:
+      " Explore the roads in style and sustainability with the Toyota Aventador, now available for daily rental. This all-electric vehicle offers a perfect blend of elegance, performance, and eco-friendliness, making it an ideal choice for various travel needs.",
+  },
+
+  {
+    id: 3,
+    brand: "BMW",
+    rating: 132,
+    carName: "BMW X3",
+    imgUrl: img03,
+    model: "Model-2022",
+    price: 65,
+    speed: "20kmpl",
+    gps: "GPS Navigation",
+    seatType: "Heated seats",
+    automatic: "Automatic",
+    description:
+      " Discover the joy of driving with the BMW X3, now available for daily rental. This all-electric vehicle combines cutting-edge technology, stunning design, and remarkable performance, making it a fantastic choice for your next eco-friendly journey.",
+  },
+
+  {
+    id: 4,
+    brand: "Nissan",
+    rating: 102,
+    carName: "Nissan Mercielago",
+    imgUrl: img04,
+    model: "Model-2022",
+    price: 70,
+    speed: "20kmpl",
+    gps: "GPS Navigation",
+    seatType: "Heated seats",
+    automatic: "Automatic",
+    description:
+      " Unleash the power of innovation with the Nissan Mercielago, now available for daily rental. This all-electric vehicle offers a unique blend of style, performance, and sustainability, making it an excellent choice for various travel needs.",
+  },
+
+  {
+    id: 5,
+    brand: "Ferrari",
+    rating: 94,
+    carName: "Ferrari Camry",
+    imgUrl: img05,
+    model: "Model-2022",
+    price: 45,
+    speed: "20kmpl",
+    gps: "GPS Navigation",
+    seatType: "Heated seats",
+    automatic: "Automatic",
+    description:
+      " Get ready to experience the future of driving with the Ferrari Camry, now available for daily rental. This all-electric vehicle combines elegance, performance, and sustainability, making it an exceptional choice for your next eco-friendly journey.",
+  },
+
+  {
+    id: 6,
+    brand: "Mercedes",
+    rating: 119,
+    carName: "Mercedes Benz XC90",
+    imgUrl: img06,
+    model: "Model-2022",
+    price: 85,
+    speed: "20kmpl",
+    gps: "GPS Navigation",
+    seatType: "Heated seats",
+    automatic: "Automatic",
+    description:
+      " Experience the future of driving with the Mercedes Benz XC90, now available for rent! This all-electric vehicle combines style, performance, and eco-friendliness, making it an excellent choice for your next local or long-distance journey.",
+  },
+
+  {
+    id: 7,
+    brand: "Audi",
+    rating: 82,
+    carName: "Audi Fiesta",
+    imgUrl: img07,
+    model: "Model 3",
+    price: 50,
+    speed: "20kmpl",
+    gps: "GPS Navigation",
+    seatType: "Heated seats",
+    automatic: "Automatic",
+    description:
+      " Discover the joy of driving with the Audi Fiesta, now available for daily rental. This all-electric vehicle combines cutting-edge technology, stunning design, and remarkable performance, making it a fantastic choice for your next eco-friendly journey.",
+  },
+
+  {
+    id: 8,
+    brand: "Colorado",
+    rating: 52,
+    carName: "Rolls Royce Colorado",
+    imgUrl: img08,
+    model: "Model 3",
+    price: 50,
+    speed: "20kmpl",
+    gps: "GPS Navigation",
+    seatType: "Heated seats",
+    automatic: "Automatic",
+    description:
+      " Explore the roads in style and sustainability with the Rolls Royce Colorado, now available for daily rental. This all-electric vehicle offers a perfect blend of elegance, performance, and eco-friendliness, making it an ideal choice for various travel needs.",
+  },
+];
+
+export default carData;
